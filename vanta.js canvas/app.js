5e547470e5019e866f0e361842041da44f850b4a
Aditya Dumbare <113708561+adityadumbare@users.noreply.github.com>
2023-02-08 19:38:35 +0530

    Update

--- vanta.js canvas/app.js
+++ vanta.js canvas/app.js
@@ -1,9 +1,27 @@
+// VANTA.BIRDS({
+//     el: "#intro",
+//     mouseControls: true,
+//     touchControls: true,
+//     minHeight: 200.00,
+//     minWidth: 200.00,
+//     scale: 1.00,
+//     scaleMobile: 1.00
+// })
+
 VANTA.BIRDS({
     el: "#intro",
     mouseControls: true,
     touchControls: true,
+    gyroControls: false,
     minHeight: 200.00,
     minWidth: 200.00,
     scale: 1.00,
-    scaleMobile: 1.00
-})
+    scaleMobile: 1.00,
+    backgroundColor: 0x80808,
+    color1: 0xf70606,
+    color2: 0xe6ff1d,
+    birdSize: 0.60,
+    wingSpan: 22.00,
+    separation: 52.00,
+    alignment: 42.00,
+    cohesion: 45.00})
